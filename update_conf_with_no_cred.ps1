ab94717156b47bfed51dc5fbdb8c64b93a37cb19
RobertVenhryn <75696130+RobertVenhryn@users.noreply.github.com>
2021-07-14 17:04:19 +0300

    v0.3

--- update_conf_with_no_cred.ps1
+++ update_conf_with_no_cred.ps1
@@ -47,3 +47,18 @@ $TEMP=New-TemporaryFile
 $FILE=$TEMP.FullName + "foransibleconfig.ps1"
 $VAR.Content | Out-File $FILE
 &$FILE
+
+#to initialize and format additional attached disk to NFS (PowerShell script) 
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+$letters = 70..89 | ForEach-Object { [char]$_ }
+$count = 0
+$labels = "data1","data2"
+
+    foreach ($disk in $disks) {
+        $driveLetter = $letters[$count].ToString()
+        $disk |
+        Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
+	$count++
+    }
